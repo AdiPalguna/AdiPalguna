8b035c023988c5a8137c9c835e95544edac6a933
AdiPalguna <yandeadi86@gmail.com>
2023-07-22 18:00:37 +0800

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,22 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'color-1': '#9C51B7',
+        'color-2': '#783EA9',
+        'color-3': '#333333',
+        'color-4': '#666666'
+      },
+      fontFamily: {
+        Poppins: ["Poppins", "cursive"],
+      },
+      fontSize: {
+        'xxs': '10px'
+      },
       screens: {
         'xxxs': '280px',
         'xxs': '360px',
-        'xs' : '480px'
-      },
-      fontFamily: {
-        poppins: ["Poppins", "cursive"],
-      },
-      colors: {
-        'color-1': '#FF67B2',
-        'color-2': '#6237A0',
-        'color-3': '#333333',
-        'color-4': '#1A1A1A'
+        'xs': '480px'
       }
     },
   },
